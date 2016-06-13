00a444610b8143df2abff4bb7f549d30cbc1c83b
David Koenig <koenigdavidmj@gmail.com>
2016-06-12 20:40:44 -0700

    Pretend to be 'find .' for the moment.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 extern crate docopt;
 extern crate rustc_serialize;
 
+use std::fs::{self, DirEntry};
+use std::io;
+use std::path::Path;
+
 const USAGE: &'static str = "
 Recursively search files for a given pattern.
 
@@ -21,9 +25,27 @@ struct Args {
     flag_word: bool,
 }
 
+fn visit_dirs<P: AsRef<Path>>(dir: P, cb: &Fn(&DirEntry)) -> io::Result<()> {
+    if try!(fs::metadata(dir.as_ref())).is_dir() {
+        for entry in try!(fs::read_dir(dir.as_ref())) {
+            let entry = try!(entry);
+            if try!(fs::metadata(entry.path())).is_dir() {
+                try!(visit_dirs(&entry.path(), cb));
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+    Ok(())
+}
+
 fn main() {
     let args: Args = docopt::Docopt::new(USAGE)
                                     .and_then(|d| d.decode())
                                     .unwrap_or_else(|e| e.exit());
-    println!("{:?}", args);
+
+    fn callback(de: &DirEntry) {
+        println!("{}", de.path().strip_prefix(".").unwrap().to_str().unwrap());
+    }
+    visit_dirs(Path::new("."), &callback);
 }
