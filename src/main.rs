4ce9ccb937c9ce547266cc860e229f7a0fa7fee9
David Koenig <koenigdavidmj@gmail.com>
2016-06-12 19:17:56 -0700

    Minimal argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate docopt;
+extern crate rustc_serialize;
+
+const USAGE: &'static str = "
+Recursively search files for a given pattern.
+
+Usage:
+  gimme [options] <pattern>
+  gimme --help
+
+Options:
+  --help                Show this screen.
+  -l --list-filenames   Only show matching filenames, not the lines.
+  -w --word             Only match whole words.
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_pattern: String,
+    flag_list_filenames: bool,
+    flag_word: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = docopt::Docopt::new(USAGE)
+                                    .and_then(|d| d.decode())
+                                    .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
 }
